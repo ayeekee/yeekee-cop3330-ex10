766e239eceff4f9fda15a52297d5a133ea99c0f2
EV-02 <ayeekee@knights.ucf.edu>
2021-09-08 22:07:44 -0400

    added more output statements

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -29,16 +29,24 @@ public class App {
 
         // convert input string to numerical data
         double priceItem1 = Double.parseDouble(price1);
-        double numItem1 = Double.parseDouble(quantity1);
+        int numItem1 = Integer.parseInt(quantity1);
         double priceItem2 = Double.parseDouble(price2);
-        double numItem2 = Double.parseDouble(quantity2);
+        int numItem2 = Integer.parseInt(quantity2);
         double priceItem3 = Double.parseDouble(price3);
-        double numItem3 = Double.parseDouble(quantity3);
+        int numItem3 = Integer.parseInt(quantity3);
 
         // calculations
-        double subtotal = (priceItem1 * numItem1) + (priceItem2 * numItem2) + (priceItem3 * numItem3);
+        double item1Total = priceItem1 * numItem1;
+        double item2Total = priceItem2 * numItem2;
+        double item3Total = priceItem3 * numItem3;
+
+        double subtotal = item1Total + item2Total + item3Total;
         double tax = subtotal * 0.055;
         double total = subtotal + tax;
+
+        System.out.println("Item 1: quantity = " + quantity1 + ", total = $" + String.format("%.2f", item1Total));
+        System.out.println("Item 2: quantity = " + quantity2 + ", total = $" + String.format("%.2f", item2Total));
+        System.out.println("Item 3: quantity = " + quantity3 + ", total = $" + String.format("%.2f", item3Total));
 
         System.out.println("Subtotal: $" + String.format("%.2f", subtotal) + "\nTax: $" + String.format("%.2f", tax) + "\nTotal: $" + String.format("%.2f", total));
     }
